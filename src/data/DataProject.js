8f12e9022e4610923c6bef5fc6b628b863d539ec
Huỳnh Dũng <@huynhdungfed>
2021-05-15 10:14:11 +0700

    day 2.1

--- src/data/DataProject.js
+++ src/data/DataProject.js
@@ -12,9 +12,19 @@ const listProject = [
     status: "",
   },
   {
-    id: 1,
-    title: "Project 2",
-    description: "Hello, đây là Project 2",
+    id: 2,
+    title: "Disney+ Clone",
+    description: "Based on Disney's API, when login the Web allows users to view movie information,...",
+    techUse: "Technologies Used: ReactJS, firebase, redux, redux toolkit, styled-component, slick-carousel",
+    imgPrj: "https://i0.wp.com/s1.uphinh.org/2021/05/14/project2.png",
+    linkPrj: "https://disney-clone-react.web.app/",
+    newTab: "_blank",
+    status: ""
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description: "Hello, đây là Project 3",
     techUse: "",
     imgPrj: "",
     linkPrj: "#",
@@ -22,9 +32,19 @@ const listProject = [
     status: "noti"
   },
   {
-    id: 1,
-    title: "Project 3",
-    description: "Hello, đây là Project 3",
+    id: 4,
+    title: "Project 4",
+    description: "Hello, đây là Project 4",
+    techUse: "",
+    imgPrj: "",
+    linkPrj: "#",
+    newTab: "",
+    status: "noti"
+  },
+  {
+    id: 5,
+    title: "Project 5",
+    description: "Hello, đây là Project 5",
     techUse: "",
     imgPrj: "",
     linkPrj: "#",
