6d77f47d1d005fd520cba65fbfcec33cd02a7516
Huỳnh Dũng <@huynhdungfed>
2021-05-16 14:44:11 +0700

    day 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
     <div>
       <HomePage />
+      <MessengerCustomerChat
+        pageId="100330828425458"
+        appId="916945135550346"
+      />
+      ,
     </div>
   );
 }
