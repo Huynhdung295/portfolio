fef198876d05adadb4d2930e5c3e7a1d564ad692
Huỳnh Dũng <@huynhdungfed>
2021-05-19 20:36:49 +0700

    day 3

--- src/components/Resume/NavbarRS/NavbarRS.js
+++ src/components/Resume/NavbarRS/NavbarRS.js
@@ -3,14 +3,18 @@ import "./NavbarRS.css";
 import dataNavbar from "./DataNavbar";
 function NavbarRS() {
   
-  const nav = document.getElementById("navRS-menu");
 
+  const toggleMenu = () => {
+    const nav = document.getElementById("navRS-menu");
+    nav.classList.toggle("show-menu");
+  };
   const data = dataNavbar.map((data) => {
     return (
       <li className="navRS__item">
         <a
           href={data.navHref}
           onClick={() => {
+            const nav = document.getElementById("navRS-menu");
             nav.classList.remove("show-menu");
           }}
           className="navRS__link"
@@ -21,10 +25,6 @@ function NavbarRS() {
     );
   });
 
-  const toggleMenu = () => {
-    nav.classList.toggle("show-menu");
-  };
-
   return (
     <header className="l-headerRS" id="header">
       <div className="navRS bd-containerRS">
@@ -34,7 +34,7 @@ function NavbarRS() {
         <div className="navRS__menu" id="navRS-menu">
           <ul className="navRS__list">{data}</ul>
         </div>
-        <div className="navRS__toggle" onClick={toggleMenu} id="navRS-toggle">
+        <div className="navRS__toggle" onClick={toggleMenu}>
           <i className="bx bx-grid-alt navRS__icon" />
         </div>
       </div>
