32a98730190ad3ab69c3abc07bedcf66df2fd913
Huỳnh Dũng <@huynhdungfed>
2021-05-19 15:57:57 +0700

    day 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import MessengerCustomerChat from "react-messenger-customer-chat";
+
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Resume from "./components/Resume/Resume";
 function App() {
@@ -14,7 +14,7 @@ function App() {
           <Resume />
         </Route>
       </Switch>
-      <MessengerCustomerChat pageId="100330828425458" appId="916945135550346" />
+      
     </Router>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import FullPage from '../settings/FullPage'
-
+import MessengerCustomerChat from "react-messenger-customer-chat";
 function HomePage() {
     return (
         <div>
             <Header/>
             <FullPage />
+            <MessengerCustomerChat pageId="100330828425458" appId="916945135550346" />
         </div>
     )
 }
